d22a22de909788445999a52aaeeac932eba8bec7
JoyKaremera <jkare104@uottawa.ca>
2024-12-01 18:09:59 -0500

    tuto 2

---
+++ app/src/main/java/com/seg2505/javaapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.seg2505.javaapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.seg2505.javaapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        EditText editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                if (!name.isEmpty()) {
+                    textView.setText("Hello, " + name + "!");
+                } else {
+                    textView.setText("Hello, World!");
+                }
+            }
+        });
+    }
+}
